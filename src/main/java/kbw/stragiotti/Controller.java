8d18cd321fda125312a8a5f739f52f87f0f0fd2b
dsaemii <@2d@UXKaGh8d66h6Jd3CcKxvDJhi>
2023-03-19 15:44:03 +0100

    Controller.java added

---
+++ src/main/java/kbw/stragiotti/Controller.java
@@ -0,0 +1,10 @@
+package kbw.stragiotti;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    private TextField d1;
+}
