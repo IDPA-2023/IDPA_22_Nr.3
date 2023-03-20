e8642bf43a4f3fb1eb1e9c298756b934d981a55e
Samira Stragiotti <samira.stragiotti@stud.kbw.ch>
2023-03-20 17:38:16 +0100

    .csv reader added

--- src/main/java/kbw/stragiotti/Main.java
+++ src/main/java/kbw/stragiotti/Main.java
@@ -1,82 +1,67 @@
 package kbw.stragiotti;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         // ToDo: daten einlesen
+        ArrayList<Fach> faecher = new ArrayList<Fach>();
+        String line = " ";
+        String splitBy = ",";
+        try {
+            // parsing a CSV file into BufferedReader class constructor
+            BufferedReader br = new BufferedReader(new FileReader("/Users/samirastragiotti/IdeaProjects/notenrechner/src/main/resources/test_file.csv"));
+            for(int i = 0; (line = br.readLine()) != null; i++) {
+                String[] faecherCSV = line.split(splitBy);
+                if (i>=1) {
+                    /*
+                    for testing reasons:
 
-
-        // 1. semester
-        double d1 = 0.0;
-        double f1 = 0.0;
-        double e1 = 0.0;
-        double m1 = 0.0;
-        double wr1 = 0.0;
-        double rw1 = 0.0;
-        double g1 = 0.0;
-        // 2. semester
-        double d2 = 0.0;
-        double f2 = 0.0;
-        double e2 = 0.0;
-        double m2 = 0.0;
-        double wr2 = 0.0;
-        double rw2 = 0.0;
-        double g2 = 0.0;
-        // 3. semester
-        double d3 = 0.0;
-        double f3 = 0.0;
-        double e3 = 0.0;
-        double m3 = 0.0;
-        double wr3 = 0.0;
-        double rw3 = 0.0;
-        double g3 = 0.0;
-        double tu3 = 0.0;
-        // 4. semester
-        double d4 = 0.0;
-        double f4 = 0.0;
-        double e4 = 0.0;
-        double m4 = 0.0;
-        double wr4 = 0.0;
-        double rw4 = 0.0;
-        double g4 = 0.0;
-        double tu4 = 0.0;
-        // 5. semester
-        double d5 = 0.0;
-        double e5 = 0.0;
-        double wr5 = 0.0;
-        double rw5 = 0.0;
-        // 6. semester
-        double d6 = 0.0;
-        double e6 = 0.0;
-        double wr6 = 0.0;
-        double rw6 = 0.0;
-        // schriftliche abschlussprüfungen
-        double d_schriftlich = 0.0;
-        double f_schriftlich = 0.0;
-        double e_schriftlich = 0.0;
-        double m_schriftlich = 0.0;
-        double wr_schriftlich = 0.0;
-        double rw_schriftlich = 0.0;
-        // mündliche abschlussprüfungen
-        double d_muendlich = 0.0;
-        double f_muendlich = 0.0;
-        double e_muendlich = 0.0;
-        // idpa
-        double idpa = 0.0;
-        // idaf
-        double idaf1 = 0.0;
-        double idaf2 = 0.0;
-
-        // fächer erstellen (beispiel)
-        Fach d = new Fach("Deutsch",true, true);
-        d.setSchriftlich(d_schriftlich);
-        d.setMuendlich(d_muendlich);
-
-        double d_pos1 = d.getPos1();
-        double d_pos2 = d.getPos2();
+                    System.out.println("[Fach=" + faecherCSV[0] +
+                            ", 1. Semester=" + faecherCSV[1] +
+                            ", 2. Semester=" + faecherCSV[2] +
+                            ", 3. Semester=" + faecherCSV[3] +
+                            ", 4. Semester= " + faecherCSV[4] +
+                            ", 5. Semester= " + faecherCSV[5] +
+                            ", 6. Semester= " + faecherCSV[6] +
+                            ", schriftlich= " + faecherCSV[7] +
+                            ", mündlich= " + faecherCSV[8] +
+                            ", start_semester= " + faecherCSV[9] +
+                            ", start_semester= " + faecherCSV[10] +
+                            ", schriftlich?= " + faecherCSV[11] +
+                            ", mündlich?= " + faecherCSV[12] +"]");
+                            */
+                    // add read data as "Fach" to faecher list
+                    faecher.add(new Fach(faecherCSV[0], Boolean.parseBoolean(faecherCSV[11]), Boolean.parseBoolean(faecherCSV[12])));
+                    ArrayList<Double> noten = new ArrayList<>(List.of(
+                            Double.parseDouble(faecherCSV[1]),
+                            Double.parseDouble(faecherCSV[2]),
+                            Double.parseDouble(faecherCSV[3]),
+                            Double.parseDouble(faecherCSV[4]),
+                            Double.parseDouble(faecherCSV[5]),
+                            Double.parseDouble(faecherCSV[6])));
+                    if(noten != null) {
+                        faecher.get(i-1).setZeugnisnoten(noten);
+                    }
+                    if(faecher.get(i-1).isSchriftlich_exist()) {
+                        faecher.get(i-1).setSchriftlich(Double.parseDouble(faecherCSV[7]));
+                    }
+                    if(faecherCSV[9] != null) {
+                        faecher.get(i-1).setStart_semester(Integer.parseInt(faecherCSV[9]));
+                    }
+                    if(faecherCSV[10] != null) {
+                        faecher.get(i-1).setEnd_semester(Integer.parseInt(faecherCSV[10]));
+                    }
+                }
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
 
         // ToDo: diagram erstellen
         // ToDo: exportieren
