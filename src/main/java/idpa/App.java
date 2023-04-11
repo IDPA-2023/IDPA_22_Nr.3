9e5c826fd9420f77318ee2f8337e6ca40a8e2712
Samira Stragiotti <samira.stragiotti@stud.kbw.ch>
2023-04-11 22:06:23 +0200

    fxml fixed

--- src/main/java/idpa/App.java
+++ src/main/java/idpa/App.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Notenrechner");
         stage.setMaximized(true);
         stage.setScene(scene);
         stage.show();
